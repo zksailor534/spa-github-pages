c7aa5329bcde18689f06604ff2ef47dfca50f3f0
zksailor534 <nate.greene@gmail.com>
2016-12-07 13:42:10 -0500

    Generate html with html-webpack-plugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,6 +1,9 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+
+import pkg from './package.json';
 
 const extractCSS = new ExtractTextPlugin('bundle.css');
 
@@ -27,24 +30,26 @@ export default {
   resolve: {
     extensions: ['', '.js', '.jsx', '.css'],
   },
-  plugins: (() => {
-    if (process.argv.indexOf('-p') !== -1) {
-      return [
-        new webpack.DefinePlugin({
-          'process.env': {
-            NODE_ENV: JSON.stringify('production'),
-          },
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-          output: {
-            comments: false,
-          },
-        }),
-        extractCSS,
-      ];
-    }
-    return [
-      extractCSS,
-    ];
-  })(),
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"development"',
+    }),
+    extractCSS,
+    new HtmlWebpackPlugin({
+      template: 'lib/index_template.ejs',
+      filename: 'index.html',
+
+      // Context for the template
+      title: `${pkg.name} - ${pkg.description}`,
+      description: pkg.description,
+    }),
+    new HtmlWebpackPlugin({
+      template: 'lib/404_template.ejs',
+      filename: '404.html',
+
+      // Context for the template
+      title: `${pkg.name} - ${pkg.description}`,
+      remote: true,
+    }),
+  ],
 };
